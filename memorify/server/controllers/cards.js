5606783bbf52b0c3a6b0a02bc34992a5767b5406
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-08 23:10:04 +0300

    Edit get cards function

--- memorify/server/controllers/cards.js
+++ memorify/server/controllers/cards.js
@@ -1,3 +1,20 @@
-export const getCards = (req, res) => {
-    res.send('Welcome!');
+import CardSchema from '../models/Card.js';
+
+export const getCards = async (req, res) => {
+    try {
+        const cards = await CardSchema.find();
+        console.log(cards);
+        res.status(200).json(cards);
+
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const createCard = (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
 }
