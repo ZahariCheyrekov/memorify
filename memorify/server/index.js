f6acb2853451285a3a3936233ede06aa6b383ead
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-08 16:19:33 +0300

    Conntect application to mongodb and add dotenv

--- memorify/server/index.js
+++ memorify/server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 
 const app = express();
@@ -8,3 +9,11 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 app.use(cors());
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(() => console.log(`Server is listening on port: ${PORT}`)))
+    .catch((error) => console.log(error));
