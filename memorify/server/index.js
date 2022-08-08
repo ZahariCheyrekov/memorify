a964a1c2d1077cb4766ee5609c42b88e18a0d1f6
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-08 16:39:50 +0300

    Configure application file

--- memorify/server/index.js
+++ memorify/server/index.js
@@ -1,19 +1,20 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 
-const app = express();
+import cardsRoutes from './routes/cards.js';
+import { configDatabase } from './config/configDatabase.js';
 
-app.use(bodyParser.urlencoded({ extended: false }))
+const app = express();
+dotenv.config();
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors());
 
-dotenv.config();
+app.use('/cards', cardsRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(() => console.log(`Server is listening on port: ${PORT}`)))
-    .catch((error) => console.log(error));
+configDatabase(app, PORT);
