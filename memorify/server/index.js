f183c072b5ffb5a79f82368ef7dfa866fa7d2129
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-08 16:00:35 +0300

    Configure server application

--- memorify/server/index.js
+++ memorify/server/index.js
@@ -3,3 +3,8 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(cors());
