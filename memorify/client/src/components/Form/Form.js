403c0aedbbd09c225bbc8d6c345e176b212f7d24
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-09 17:39:59 +0300

    Edit form component

--- memorify/client/src/components/Form/Form.js
+++ memorify/client/src/components/Form/Form.js
@@ -1,40 +1,53 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { createCard } from '../../services/cards';
 
+import './Form.css';
+
 const Form = () => {
+    const navigate = useNavigate();
     const [cardData, setCardData] = useState({ author: '', title: '', description: '', tags: '', selectedFile: '' });
+
 
     const handleSubmit = async (ev) => {
         ev.preventDefault();
+
         createCard(cardData);
+        ev.target.reset();
+
+        navigate('/cards');
     }
 
     const clearForm = (ev) => {
         ev.preventDefault();
-        ev.target.parentNode.reset();
+        ev.target.parentNode.parentNode.reset();
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <legend>Add Album</legend>
+        <main className="main__create">
+            <form className="main__form" onSubmit={handleSubmit}>
+                <legend>Create card</legend>
 
-            <label htmlFor="author" className="vhide">Author</label>
-            <input id="author" name="author" className="author" type="text" placeholder="Author" onChange={(ev) => setCardData({ ...cardData, author: ev.target.value })} />
+                <label htmlFor="author" className="main__form--label">Author</label>
+                <input id="author" name="author" className="author" type="text" placeholder="John" required onChange={(ev) => setCardData({ ...cardData, author: ev.target.value })} />
 
-            <label htmlFor="title" className="vhide">Title</label>
-            <input id="title" name="title" className="title" type="text" placeholder="Title" onChange={(ev) => setCardData({ ...cardData, title: ev.target.value })} />
+                <label htmlFor="title" className="main__form--label">Title</label>
+                <input id="title" name="title" className="title" type="text" placeholder="Summer party" required onChange={(ev) => setCardData({ ...cardData, title: ev.target.value })} />
 
-            <label htmlFor="description" className="vhide">Description</label>
-            <textarea id="description" name="description" className="description" type="text" placeholder="Description" onChange={(ev) => setCardData({ ...cardData, description: ev.target.value })} />
+                <label htmlFor="description" className="main__form--label">Description</label>
+                <textarea id="description" name="description" className="description" type="text" placeholder="This summer was..." required onChange={(ev) => setCardData({ ...cardData, description: ev.target.value })} />
 
-            <label htmlFor="tags" className="vhide">Tags</label>
-            <input id="tags" name="tags" className="tags" type="text" placeholder="Tags" onChange={(ev) => setCardData({ ...cardData, tags: ev.target.value })} />
+                <label htmlFor="tags" className="main__form--label">Tags</label>
+                <input id="tags" name="tags" className="tags" type="text" placeholder="summer, party, fun" required onChange={(ev) => setCardData({ ...cardData, tags: ev.target.value })} />
 
-            <input id="file" name="file" className="file" type="file" accept="Image/*" onChange={(ev) => setCardData({ ...cardData, selectedFile: ev.target.value })} />
+                <input id="file" name="file" className="main__form--label" type="file" accept="Image/*" required onChange={(ev) => setCardData({ ...cardData, selectedFile: ev.target.value })} />
 
-            <button className="add-card" type="submit">Create card</button>
-            <button className="clear-form" onClick={clearForm}>Clear</button>
-        </form>
+                <div className="main__form--buttons">
+                    <button className="main__form--add--card" type="submit">Create</button>
+                    <button className="main__form--clear--form" onClick={clearForm}>Clear</button>
+                </div>
+            </form>
+        </main>
     );
 }
 
