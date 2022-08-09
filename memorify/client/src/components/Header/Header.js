ef6c8edb7fb3c5ad5456f25b4455d964da9be876
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-09 15:42:53 +0300

    Add header cards link

--- memorify/client/src/components/Header/Header.js
+++ memorify/client/src/components/Header/Header.js
@@ -4,12 +4,13 @@ import { Link } from 'react-router-dom';
 export const Header = () => {
     return (
         <header className="header">
-            <h1>
+            <h1 className="header__title">
                 <Link to={'/'}>Memorify</Link>
             </h1>
 
-            <nav>
-                <ul>
+            <nav className="header__nav">
+                <ul className="header__ul">
+                    <Link to={'/cards'}>Cards</Link>
                     <Link to={'/create'}>Create</Link>
                     <Link to={'/login'}>Login</Link>
                     <Link to={'/register'}>Register</Link>
