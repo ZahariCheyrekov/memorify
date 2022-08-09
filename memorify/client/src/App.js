93f6f6aa9f8f9fbfa786460ac77229fc78d2e258
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-09 15:03:36 +0300

    Edit app component

--- memorify/client/src/App.js
+++ memorify/client/src/App.js
@@ -1,19 +1,12 @@
 import './App.css';
-import { AppBar, Container, Typography } from '@mui/material';
+import './index.css';
+
 import Cards from './components/Cards/Cards';
 import Form from './components/Form/Form';
 
 function App() {
     return (
-        <Container maxWidth="lg">
-            <AppBar position="static">
-                <Typography variant="h1">
-                    Memorify
-                </Typography>
-            </AppBar>
-            <Cards />
-            <Form />
-        </Container>
+        <Cards />
     );
 }
 
