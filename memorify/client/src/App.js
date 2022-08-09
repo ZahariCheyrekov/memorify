dd4695be97048f14a810abaf7eadf5d539e98a06
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-09 15:20:23 +0300

    Import header functional component

--- memorify/client/src/App.js
+++ memorify/client/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import './index.css';
 
+import Header from './components/Header/Header';
 import Cards from './components/Cards/Cards';
 import Form from './components/Form/Form';
 
 function App() {
     return (
-        <Cards />
+        <>
+            <Header />
+            <Cards />
+        </>
     );
 }
 
