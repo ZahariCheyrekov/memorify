0276f4644248e596295b89c473484ff44d707c09
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-09 15:21:02 +0300

    Create header component

---
+++ memorify/client/src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import './Header.css';
+import { Link } from 'react-router-dom';
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <h1>Memorify</h1>
+
+            <nav>
+                <ul>
+                    <Link to={'/create'}>Create</Link>
+                    <Link to={'/login'}>Login</Link>
+                    <Link to={'/register'}>Register</Link>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
