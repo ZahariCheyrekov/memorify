eefe7998611ca6bef426e77b41b1766248e61ac0
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-09 15:42:42 +0300

    Add app routes

--- memorify/client/src/App.js
+++ memorify/client/src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import './index.css';
 
+import { Route, Routes } from 'react-router-dom';
+
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 import Cards from './components/Cards/Cards';
 import Form from './components/Form/Form';
 
@@ -9,7 +12,14 @@ function App() {
     return (
         <>
             <Header />
-            <Cards />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/create' element={<Form />} />
+                <Route path='/cards' element={<Cards />} />
+                <Route />
+                {/* <Route path='/login' element={<Login />} /> */}
+                {/* <Route path='/register' element={<Register />} /> */}
+            </Routes>
         </>
     );
 }
