51a949724c4727f937c5956fc44b1daef9b90edd
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-09 22:40:30 +0300

    Fix auth form component structure

--- memorify/client/src/components/Auth/Auth.js
+++ memorify/client/src/components/Auth/Auth.js
@@ -3,6 +3,7 @@ import './Auth.css';
 
 const Auth = () => {
     const [isSignUp, setIsSignUp] = useState(null);
+    const [showPassword, setSowPassword] = useState(false);
 
     const handleSubmit = (ev) => {
         ev.preventDefault();
@@ -10,10 +11,16 @@ const Auth = () => {
 
     return (
         <form className="form__auth" onSubmit={handleSubmit}>
-            <input id="email" name="email" className="email" type="text" placeholder="example@gmail.com *" required />
+            <legend>Login</legend>
 
-            <input id="password" name="password" className="password" type="password" placeholder="Password *" required />
-            <i className="fa-solid fa-eye"></i>
+            <input id="email" name="email" className="email" type="text" placeholder="Email Address *" required />
+
+            <label className="form__label--password" htmlFor="password">
+                <input id="password" name="password" className="password" type={showPassword ? "text" : "password"} placeholder="Password *" required />
+                <i className="fa-solid fa-eye" onClick={() => setSowPassword(state => !state)}></i>
+            </label>
+
+            <button className="form__button--submit" type="submit">Login</button>
         </form>
     );
 }
