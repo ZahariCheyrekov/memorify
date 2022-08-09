7c0dc94e3b6597561a2ca84ba2a3a638ad62db6b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-09 16:22:46 +0300

    Create home component structure

--- memorify/client/src/components/Home/Home.js
+++ memorify/client/src/components/Home/Home.js
@@ -1,8 +1,42 @@
+import './Home.css';
+import beachSunglasses from '../../assets/beach-sunglasses.jpg';
+
 const Home = () => {
     return (
-        <h1>
-            Home
-        </h1>
+        <main className="home__main">
+            <div className="home__body">
+                <h1 className="home__title">
+                    Welcome to Memorify
+                </h1>
+
+                <p className="home__content">
+                    A place where people can share more about their favorite memories
+                    with others and have a great time!
+                </p>
+
+                <ul className="home__ul">
+                    <li className="home__li--item">
+                        Explore other people memories
+                    </li>
+                    <li className="home__li--item">
+                        Share your memories
+                    </li>
+                    <li className="home__li--item">
+                        Like other memories
+                    </li>
+                    <li className="home__li--item">
+                        Have fun
+                    </li>
+                    <li className="home__li--item">
+                        Learn new things
+                    </li>
+                </ul>
+            </div>
+
+            <article className="home__img">
+                <img src={beachSunglasses} alt="sunglasses" />
+            </article>
+        </main>
     );
 }
 
