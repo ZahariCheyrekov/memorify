730f4114cf8c67abe85a367e0fe09da957f2eff4
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-09 15:04:35 +0300

    Create card list

--- memorify/client/src/components/Cards/Cards.js
+++ memorify/client/src/components/Cards/Cards.js
@@ -1,22 +1,39 @@
 import { useEffect, useState } from 'react';
+
+import Card from './Card/Card';
 import { getCards } from '../../services/cards';
 
 const Cards = () => {
     const [cards, setCards] = useState([]);
 
     useEffect(() => {
-        const fetchCards = async () => {
-            const cards = await getCards();
-            setCards(cards);
-        }
-
         fetchCards();
-        console.log(cards);
     }, []);
 
+    const fetchCards = async () => {
+        const cards = await getCards();
+        setCards(cards);
+    }
+
     return (
-        <>
-        </>
+        <ul className="cards">
+            {cards ?
+                cards.map(card =>
+                    <Card
+                        key={card._id}
+                        id={card._id}
+                        author={card.author}
+                        createdAt={card.createdAt}
+                        description={card.description}
+                        likeCount={card.likeCount}
+                        selectedFile={card.selectedFile}
+                        tags={card.tags}
+                        title={card.title}
+                    />
+                )
+                : ' No cards'
+            }
+        </ul>
     );
 }
 
