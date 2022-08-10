fb9a78c537167db5da32828b1fb95e479531e25a
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-10 16:35:01 +0300

    Fix request urls

--- memorify/client/src/api/requester.js
+++ memorify/client/src/api/requester.js
@@ -4,9 +4,9 @@ import { baseUrl, cardsUrl } from '../constants/urls';
 
 export const fetchCards = () => axios.get(cardsUrl);
 export const createCard = (cardData) => axios.post(cardsUrl, cardData);
-export const updateCard = (id, cardData) => axios.patch(`${baseUrl}/${id}`, cardData);
-export const likeCard = (id) => axios.patch(`${baseUrl}/${id}/likeCard`);
-export const deleteCard = (id) => axios.delete(`${baseUrl}/${id}`);
+export const updateCard = (id, cardData) => axios.patch(`${cardsUrl}/${id}`, cardData);
+export const likeCard = (id) => axios.patch(`${cardsUrl}/${id}/likeCard`);
+export const deleteCard = (id) => axios.delete(`${cardsUrl}/${id}`);
 
-export const signin = (data) => axios.post(`/users/sigin`, data);
-export const signup = (data) => axios.post(`/users/signup`, data);
+export const signin = (data) => axios.post(`${baseUrl}/user/signin`, data);
+export const signup = (data) => axios.post(`${baseUrl}/user/signup`, data);
