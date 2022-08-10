c7ed0f6f39b23f05df710fad472ef3ff6434291b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-10 12:13:04 +0300

    Import auth middleware and add it to card routes

--- memorify/server/routes/cards.js
+++ memorify/server/routes/cards.js
@@ -1,11 +1,15 @@
 import express from 'express';
 
+import auth from '../middlewares/auth.js';
 import { createCard, getCards, updateCard } from '../controllers/cards.js';
 
 const router = express.Router();
 
 router.get('/', getCards);
-router.post('/', createCard);
-router.patch('/:id', updateCard);
+router.post('/', auth, createCard);
+// router.get('/:id', getCard);
+router.patch('/:id', auth, updateCard);
+// router.delete('/:id', auth, deleteCard);
+// router.patch('/:id/likeCard', auth, likeCard);
 
 export default router;
