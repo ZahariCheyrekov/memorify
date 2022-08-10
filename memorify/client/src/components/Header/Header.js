4dbed5abf97abdec8e63ef14eb2a6ce66cc8862a
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-10 22:59:14 +0300

    Create header component useEffect function

--- memorify/client/src/components/Header/Header.js
+++ memorify/client/src/components/Header/Header.js
@@ -1,7 +1,17 @@
 import './Header.css';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 export const Header = () => {
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
+
+    useEffect(() => {
+        const token = user?.token;
+        setUser(JSON.parse(localStorage.getItem('user')));
+    }, [user?.token]);
+
+    const logout = () => localStorage.clear();
+
     return (
         <header className="header">
             <h2 className="header__title">
@@ -11,8 +21,14 @@ export const Header = () => {
             <nav className="header__nav">
                 <ul className="header__ul">
                     <Link to={'/memories'}>Memories</Link>
-                    <Link to={'/create'}>Create</Link>
-                    <Link to={'/auth'}>Sign In</Link>
+                    {user
+                        ?
+                        <>
+                            <Link to={'/create'}>Create</Link>
+                            <Link onClick={logout} to={'/'}>Logout</Link>
+                        </>
+                        : <Link to={'/auth'}>Sign In</Link>
+                    }
                 </ul>
             </nav>
         </header>
