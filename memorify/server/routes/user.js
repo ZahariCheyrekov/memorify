17403c25468ca28b889165d0abf1dcd8e5c74a84
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-10 16:37:21 +0300

    Add get users route

--- memorify/server/routes/user.js
+++ memorify/server/routes/user.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
-import { signin, signup } from '../controllers/user.js';
+import { getUsers, signin, signup } from '../controllers/user.js';
 
 const router = express.Router();
 
+router.get('/', getUsers);
 router.post('/signup', signup);
 router.post('/signin', signin);
 
