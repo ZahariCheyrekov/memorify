985688a244b9ce423e2f4cdb934b2512ad7fb4ca
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-10 11:27:02 +0300

    Create one auth component for login and register

--- memorify/client/src/components/Auth/Auth.js
+++ memorify/client/src/components/Auth/Auth.js
@@ -1,39 +1,73 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import './Auth.css';
 
 const Auth = () => {
-    const [isSignUp, setIsSignUp] = useState(null);
+    const navigate = useNavigate();
+    const [isSignIn, setIsSignIn] = useState(true);
     const [showPassword, setSowPassword] = useState(false);
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        repeatPassword: ''
+    });
 
     const handleSubmit = (ev) => {
         ev.preventDefault();
+
+        console.log(formData);
+    }
+
+    const handleChange = (ev) => {
+        setFormData({ ...formData, [ev.target.name]: ev.target.value })
+    }
+
+    const switchMode = () => {
+        setIsSignIn((prevState) => !prevState);
     }
 
     return (
         <form className="form__auth" onSubmit={handleSubmit}>
             <i className="fa-solid fa-lock"></i>
-            <legend className="form__auth--legend">Sign In</legend>
+            <legend className="form__auth--legend">
+                {isSignIn ? "Sign In" : "Sign Up"}
+            </legend>
 
-            <input id="email" name="email" className="email" type="text" placeholder="Email Address *" required />
+            {!isSignIn && (
+                <>
+                    <input id="firstName" name="firstName" className="firstName" type="text" placeholder="First Name *" required onChange={handleChange} />
+                    <input id="lastName" name="lastName" className="lastName" type="text" placeholder="Last Name *" required onChange={handleChange} />
+                </>
+            )}
+
+            <input id="email" name="email" className="email" type="text" placeholder="Email Address *" required onChange={handleChange} />
 
             <label className="form__label--password" htmlFor="password">
-                <input id="password" name="password" className="password" type={showPassword ? "text" : "password"} placeholder="Password *" required />
+                <input id="password" name="password" className="password" type={showPassword ? "text" : "password"} placeholder="Password *" required onChange={handleChange} />
                 <i className="fa-solid fa-eye" onClick={() => setSowPassword((state) => !state)}></i>
             </label>
 
-            <button className="form__button--signin" type="submit">SIGN IN</button>
-            <button className="form__button--signin google">
-                <i className="fa-brands fa-google"></i>
-                &nbsp;
-                GOOGLE SIGN IN
+            {!isSignIn && (
+                <label className="form__label--password" htmlFor="repeatPassword">
+                    <input id="repeatPassword" name="repeatPassword" className="repeatPassword" type={showPassword ? "text" : "password"} placeholder="Repeat Password *" required onChange={handleChange} />
+                    <i className="fa-solid fa-eye" onClick={() => setSowPassword((state) => !state)}></i>
+                </label>
+            )}
+
+            <button className="form__button--signin" type="submit">
+                {isSignIn ? "SIGN IN" : "SIGN UP"}
             </button>
 
-            <button className="form__auth--action--button">
-                DONT'T HAVE AN ACCOUNT? SIGH UP
+            <button className="form__auth--action--button" onClick={switchMode}>
+                {isSignIn
+                    ? "DONT'T HAVE AN ACCOUNT? SIGN UP"
+                    : "ALREADY HAVE AN ACCOUNT? SIGN IN"
+                }
             </button>
-        </form>
+        </form >
     );
 }
 
