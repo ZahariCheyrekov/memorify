4183934dcacb5424d367a0d76127e3f754285e5d
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-10 11:31:46 +0300

    Import user routes and use them

--- memorify/server/index.js
+++ memorify/server/index.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 
-import cardsRoutes from './routes/cards.js';
+import { cardsRoutes, userRoutes } from './routes/cards.js';
 import { configDatabase } from './config/configDatabase.js';
 
 const app = express();
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors());
 
 app.use('/memories', cardsRoutes);
+app.use('/user', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
