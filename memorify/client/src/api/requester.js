398543d49fd104ed1dbd301029f3bccd24522a60
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-11 22:26:29 +0300

    Fix like and delete card axios requests

--- memorify/client/src/api/requester.js
+++ memorify/client/src/api/requester.js
@@ -15,8 +15,8 @@ export const fetchCards = () => API.get(cardsUrl);
 export const fetchCard = (id) => API.get(`memories/${id}`);
 export const createCard = (cardData) => API.post('/memories', cardData);
 export const updateCard = (id, cardData) => API.patch(`/memories/${id}`, cardData);
-export const likeCard = (id) => API.patch(`${cardsUrl}/${id}/likeCard`);
-export const deleteCard = (id) => API.delete(`/memories/${id}`);
+export const likeCard = (id) => API.patch(`/memories/${id}/likeCard`);
+export const deleteCard = (id, navigate) => API.delete(`/memories/${id}`).then(() => navigate('/memories'));
 
 export const signin = (data) => API.post(`/user/signin`, data);
 export const signup = (data) => API.post(`/user/signup`, data);
