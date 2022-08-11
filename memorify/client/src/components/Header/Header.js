84796ab8cf96f81359eacdf529aa366115f457aa
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-11 09:56:41 +0300

    Fix jwt token expired problem

--- memorify/client/src/components/Header/Header.js
+++ memorify/client/src/components/Header/Header.js
@@ -1,16 +1,30 @@
-import './Header.css';
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+
+import './Header.css';
+import decode from 'jwt-decode';
 
 export const Header = () => {
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
 
     useEffect(() => {
         const token = user?.token;
+
+        if (token) {
+            const decodedToken = decode(token);
+
+            if (decodedToken.exp * 1000 < new Date().getTime()) {
+                logout();
+            }
+        }
+
         setUser(JSON.parse(localStorage.getItem('user')));
     }, [user?.token]);
 
-    const logout = () => localStorage.clear();
+    const logout = () => {
+        localStorage.clear();
+        setUser(null);
+    }
 
     return (
         <header className="header">
