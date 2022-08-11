7309e8e351b4182bff6145ca77eaf023b7fcd824
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-11 10:44:16 +0300

    Fix auth middleware

--- memorify/server/middlewares/auth.js
+++ memorify/server/middlewares/auth.js
@@ -1,7 +1,6 @@
 import jwt from 'jsonwebtoken';
 
 export const auth = async (req, res, next) => {
-    console.log('Hi')
     try {
         const token = req.headers.authorization.split(' ')[1];
         const isCustomAuth = token.length < 500;
