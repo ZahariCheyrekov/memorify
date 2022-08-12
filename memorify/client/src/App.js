a0df634262d8820a3fac969742d1a638192becd8
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-12 10:53:27 +0300

    Import create card and edit card components

--- memorify/client/src/App.js
+++ memorify/client/src/App.js
@@ -7,9 +7,10 @@ import { AuthContextProvider } from './contexts/AuthContext';
 
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
-import Form from './components/Form/Form';
+import CreateCard from './components/Cards/CreateCard/CreateCard';
 import Cards from './components/Cards/Cards';
 import CardDetails from './components/Cards/CardDetails/CardDetails';
+import EditCard from './components/Cards/EditCard/EditCard';
 import Auth from './components/Auth/Auth';
 
 function App() {
@@ -18,9 +19,10 @@ function App() {
             <Header />
             <Routes>
                 <Route path='/' element={<Home />} />
-                <Route path='/create' element={<Form />} />
+                <Route path='/create' element={<CreateCard />} />
                 <Route path='/memories' element={<Cards />} />
                 <Route path='/memories/:id' element={<CardDetails />} />
+                <Route path='/memories/:id/edit' element={<EditCard />} />
                 <Route path='/auth' element={<Auth />} />
             </Routes>
         </AuthContextProvider>
