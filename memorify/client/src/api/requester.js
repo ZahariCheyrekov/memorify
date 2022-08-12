e4636a887ab6189f9b5c6b05cc83527a03de7737
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-12 23:48:33 +0300

    Create post comment axios request

--- memorify/client/src/api/requester.js
+++ memorify/client/src/api/requester.js
@@ -19,7 +19,8 @@ export const fetchCards = () => API.get(cardsUrl);
 export const fetchCard = (id) => API.get(`/memories/${id}`);
 export const createCard = (cardData) => API.post('/memories', cardData);
 export const updateCard = (id, cardData) => API.patch(`/memories/${id}`, cardData);
-export const likeCard = (id) => API.patch(`/memories/${id}/likeCard`);
+export const likeCard = (id) => API.post(`/memories/${id}/likeCard`);
+export const postComment = (id, comment) => API.post(`/memories/${id}/comments`, { comment });
 export const deleteCard = (id, navigate) => API.delete(`/memories/${id}`).then(() => navigate('/memories'));
 
 export const signin = (data) => API.post(`/user/signin`, data);
