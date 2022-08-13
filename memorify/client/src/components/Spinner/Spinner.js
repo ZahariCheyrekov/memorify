16080a2a6088d9b95fe7f3ecebcd192665d2489c
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-13 17:57:51 +0300

    Create spinner component

---
+++ memorify/client/src/components/Spinner/Spinner.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import ReactLoading from 'react-loading';
+
+const Spinner = () => {
+    const styles = {
+        margin: "0 auto",
+        display: "flex",
+        width: "100px",
+        height: "70vh",
+        fill: "#ffffff"
+    }
+
+    return (
+        < ReactLoading
+            type="spin"
+            style={styles}
+        />
+    );
+}
+
+export default Spinner;
