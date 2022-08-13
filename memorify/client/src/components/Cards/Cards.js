d66d625f7c095edee6b882d69a57d01b0e31b4bf
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-13 18:00:56 +0300

    Import spinner and implement logic with it

--- memorify/client/src/components/Cards/Cards.js
+++ memorify/client/src/components/Cards/Cards.js
@@ -4,36 +4,47 @@ import './Cards.css';
 import Card from './Card/Card';
 
 import { getCards } from '../../services/cards';
+import Spinner from '../Spinner/Spinner';
 
 const Cards = () => {
+    const [loading, setLoading] = useState(false);
     const [cards, setCards] = useState([]);
 
     useEffect(() => {
+        setLoading(true);
         fetchCards();
     }, []);
 
     const fetchCards = async () => {
         const cards = await getCards();
         setCards(cards);
+
+        setLoading(false);
     }
 
     return (
-        <ul className="cards">
-            {cards.length > 0
-                ? cards.map(card =>
-                    <Card
-                        key={card._id}
-                        id={card._id}
-                        name={card.name}
-                        createdAt={card.createdAt}
-                        url={card.url}
-                        tags={card.tags}
-                        title={card.title}
-                    />
-                )
-                : <h1>No cards</h1>
+        <>
+            {loading
+                ? <Spinner />
+                :
+                <ul className="cards">
+                    {cards.length > 0
+                        ? cards.map(card =>
+                            <Card
+                                key={card._id}
+                                id={card._id}
+                                name={card.name}
+                                createdAt={card.createdAt}
+                                url={card.url}
+                                tags={card.tags}
+                                title={card.title}
+                            />
+                        )
+                        : <h1>No cards</h1>
+                    }
+                </ul>
             }
-        </ul>
+        </>
     );
 }
 
