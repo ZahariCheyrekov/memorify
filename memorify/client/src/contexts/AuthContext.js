feacf405f80fbd26d24bbad3824f47ceef19d0f2
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-13 15:04:51 +0300

    Fix auth context

--- memorify/client/src/contexts/AuthContext.js
+++ memorify/client/src/contexts/AuthContext.js
@@ -1,16 +1,18 @@
 import { createContext, useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import { getUser } from '../utils/localStorage';
 
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
+    const location = useLocation();
     const [currentUser, setCurrentUser] = useState({});
 
     useEffect(() => {
         const user = getUser();
         setCurrentUser(user);
-    }, []);
+    }, [location]);
 
     return (
         <AuthContext.Provider
