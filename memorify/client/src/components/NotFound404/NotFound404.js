9a280bfcfe1cb6c7ea2db97c3be09755a0a5d1c5
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-13 17:22:17 +0300

    Fix comment section margin

---
+++ memorify/client/src/components/NotFound404/NotFound404.js
@@ -0,0 +1,19 @@
+import './NotFound404.css';
+
+const NotFound404 = () => {
+    return (
+        <section className="section__not--found">
+            <h1 className="not__found--code">
+                404
+            </h1>
+            <h3 className="not__found--error">
+                Not Found
+            </h3>
+            <h4 className="not__found--error--message">
+                The resource requested could not be found on this server  :(
+            </h4>
+        </section>
+    );
+}
+
+export default NotFound404;
