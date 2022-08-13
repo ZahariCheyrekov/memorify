fac93871e4199e8492f14924cbd852fc6659cee3
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-13 17:38:56 +0300

    Import protected routes component

--- memorify/client/src/App.js
+++ memorify/client/src/App.js
@@ -8,11 +8,12 @@ import { AuthContextProvider } from './contexts/AuthContext';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import CreateCard from './components/Cards/CreateCard/CreateCard';
+import EditCard from './components/Cards/EditCard/EditCard';
 import Cards from './components/Cards/Cards';
 import CardDetails from './components/Cards/CardDetails/CardDetails';
-import EditCard from './components/Cards/EditCard/EditCard';
 import Auth from './components/Auth/Auth';
 import NotFound404 from './components/NotFound404/NotFound404';
+import ProtectedRoutes from './components/ProtectedRoutes/ProtectedRoutes';
 
 function App() {
     return (
@@ -20,10 +21,12 @@ function App() {
             <Header />
             <Routes>
                 <Route path='/' element={<Home />} />
-                <Route path='/create' element={<CreateCard />} />
+                <Route element={<ProtectedRoutes />}>
+                    <Route path='/create' element={<CreateCard />} />
+                    <Route path='/memories/:id/edit' element={<EditCard />} />
+                </Route>
                 <Route path='/memories' element={<Cards />} />
                 <Route path='/memories/:id' element={<CardDetails />} />
-                <Route path='/memories/:id/edit' element={<EditCard />} />
                 <Route path='/auth' element={<Auth />} />
                 <Route path='*' element={<NotFound404 />} />
             </Routes>
