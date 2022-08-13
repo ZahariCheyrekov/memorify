c7e0d8868a02b532b031683728e1c3a1f911dc76
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-13 23:36:39 +0300

    Fix nav bar links and add handle click function

--- memorify/client/src/components/Header/Navbar/Navbar.js
+++ memorify/client/src/components/Header/Navbar/Navbar.js
@@ -1,26 +1,68 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { AuthContext } from '../../../contexts/AuthContext';
 import { removeUser } from '../../../utils/localStorage';
 
+import './Navbar.css';
+
 const Navbar = () => {
     const user = useContext(AuthContext);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClick = () => {
+        setIsOpen((prevState) => !prevState);
+    }
 
     return (
         <nav className="header__nav">
-            <ul className="header__ul">
-                <Link to={'/memories'}>Memories</Link>
+            <ul className={`header__ul ${isOpen && "open"}`}>
+                <Link
+                    to={'/'}
+                    onClick={handleClick}
+                >
+                    Home
+                </Link>
+                <Link
+                    to={'/memories'}
+                    onClick={handleClick}
+                >
+                    Memories
+                </Link>
+
                 {user
                     ?
                     <>
-                        <Link to={'/create'}>Create</Link>
-                        <Link onClick={() => removeUser()} to={'/'}>Logout</Link>
+                        <Link
+                            to={'/create'}
+                            onClick={handleClick}
+                        >
+                            Create
+                        </Link>
+                        <Link onClick={() => {
+                            setIsOpen((prevState) => !prevState);
+                            removeUser();
+                        }}
+                            to={'/'}>
+                            Logout
+                        </Link>
                     </>
-                    : <Link to={'/auth'}>Sign In</Link>
+                    : <Link
+                        to={'/auth'}
+                        onClick={handleClick}
+                    >
+                        Sign In
+                    </Link>
                 }
             </ul>
-        </nav>
+            <div
+                className={`nav__toggle ${isOpen && "open"}`}
+                onClick={handleClick}
+            >
+                <div className="bar">
+                </div>
+            </div>
+        </nav >
     );
 }
 
