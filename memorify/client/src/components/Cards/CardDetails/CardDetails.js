10cbb8ec3d1ae85f5f7eefa5b69a23490be92577
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-13 17:58:37 +0300

    Import spinner and implement logic with it

--- memorify/client/src/components/Cards/CardDetails/CardDetails.js
+++ memorify/client/src/components/Cards/CardDetails/CardDetails.js
@@ -6,18 +6,22 @@ import './CardDetails.css';
 import { getCard } from '../../../services/cards';
 import { deleteCard, likeCard } from '../../../api/requester';
 import { AuthContext } from '../../../contexts/AuthContext';
+
 import CommentSection from './CommentSection/CommentSection';
+import Spinner from '../../Spinner/Spinner';
 
 const CardDetails = () => {
     const navigate = useNavigate();
     const user = useContext(AuthContext);
     const { id } = useParams();
+    const [loading, setLoading] = useState(false);
     const [card, setCard] = useState({});
     const [tags, setTags] = useState([]);
     const [likes, setLikes] = useState([]);
     const isOwner = user?.result?._id === card.creatorId;
 
     useEffect(() => {
+        setLoading(true);
         fetchCard();
     }, []);
 
@@ -27,6 +31,8 @@ const CardDetails = () => {
         setTags(card.tags[0].split(/,\s+/));
         setLikes(card.likes);
         setCard(card);
+
+        setLoading(false);
     }
 
     const handleLike = async () => {
@@ -36,64 +42,70 @@ const CardDetails = () => {
     }
 
     return (
-        <main className="main__card--details">
-            <section className="main__section">
-                <div className="section__card--details">
-                    <h1 className="section__card--title">
-                        {card.title}
-                    </h1>
-                    <div className="section__card--author">
-                        <strong>Created by:&nbsp;</strong> {card.name}&nbsp;/&nbsp;
+        <>
+            {loading
+                ? <Spinner />
+                :
+                <main className="main__card--details">
+                    <section className="main__section">
+                        <div className="section__card--details">
+                            <h1 className="section__card--title">
+                                {card.title}
+                            </h1>
+                            <div className="section__card--author">
+                                <strong>Created by:&nbsp;</strong> {card.name}&nbsp;/&nbsp;
 
-                        <p className="section__card--createdat">
-                            {new Date(card.createdAt).toDateString()}
-                        </p>
-                    </div>
-                    <p className="section__card--tags">
-                        <strong>Tags:</strong>
-                        &nbsp;
-                        <span className="section__card--span--tags">
-                            {tags &&
-                                tags.map(tag =>
-                                    <span id={tag} key={tag}>
-                                        #{tag}
-                                        &nbsp;
-                                    </span>
-                                )}
-                        </span>
-                    </p>
-                    <p className="section__card--description">
-                        {card.description}
-                    </p>
-                </div>
+                                <p className="section__card--createdat">
+                                    {new Date(card.createdAt).toDateString()}
+                                </p>
+                            </div>
+                            <p className="section__card--tags">
+                                <strong>Tags:</strong>
+                                &nbsp;
+                                <span className="section__card--span--tags">
+                                    {tags &&
+                                        tags.map(tag =>
+                                            <span id={tag} key={tag}>
+                                                #{tag}
+                                                &nbsp;
+                                            </span>
+                                        )}
+                                </span>
+                            </p>
+                            <p className="section__card--description">
+                                {card.description}
+                            </p>
+                        </div>
 
-                <article className="section__article--img">
-                    <img src={card.url} alt="memory" />
-                </article>
+                        <article className="section__article--img">
+                            <img src={card.url} alt="memory" />
+                        </article>
 
-                <section className="section__aside--actions">
-                    <i className={(isOwner || !user) ? 'fa-solid fa-heart gray' : 'fa-solid fa-heart'} onClick={() => {
-                        if (!isOwner) {
-                            handleLike();
-                        }
-                    }}>
-                        <span className="card__likes--span">
-                            {likes.length}
-                        </span>
-                    </i>
-                    {isOwner && (
-                        <>
-                            <Link to={`/memories/${id}/edit`}>
-                                <i className="fa-solid fa-pen-to-square"></i>
-                            </Link>
-                            <i className="fa-solid fa-trash" onClick={() => deleteCard(id, navigate)}></i>
-                        </>
-                    )}
-                </section>
-            </section>
+                        <section className="section__aside--actions">
+                            <i className={(isOwner || !user) ? 'fa-solid fa-heart gray' : 'fa-solid fa-heart'} onClick={() => {
+                                if (!isOwner) {
+                                    handleLike();
+                                }
+                            }}>
+                                <span className="card__likes--span">
+                                    {likes.length}
+                                </span>
+                            </i>
+                            {isOwner && (
+                                <>
+                                    <Link to={`/memories/${id}/edit`}>
+                                        <i className="fa-solid fa-pen-to-square"></i>
+                                    </Link>
+                                    <i className="fa-solid fa-trash" onClick={() => deleteCard(id, navigate)}></i>
+                                </>
+                            )}
+                        </section>
+                    </section>
 
-            <CommentSection />
-        </main >
+                    <CommentSection />
+                </main >
+            }
+        </>
     );
 }
 
