fdae4d6738f431e0f9f7a9bc25becfdfb979bd60
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-14 00:15:23 +0300

    Add app use express json

--- memorify/server/index.js
+++ memorify/server/index.js
@@ -12,6 +12,7 @@ import { DEFAULT_PORT } from './constants/index.js';
 const app = express();
 dotenv.config();
 
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors());
