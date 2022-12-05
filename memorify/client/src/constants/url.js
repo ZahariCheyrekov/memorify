370cd4c48ddec9ca9322c1eea9cc0c11dc7e378e
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-05 18:38:00 +0200

    Fix url

--- memorify/client/src/constants/url.js
+++ memorify/client/src/constants/url.js
@@ -1,3 +1,3 @@
-export const baseUrl = 'https://ruby-gorgeous-spider.cyclic.app/';
+export const baseUrl = 'https://ruby-gorgeous-spider.cyclic.app';
 
 export const cardsUrl = `${baseUrl}/memories`;
